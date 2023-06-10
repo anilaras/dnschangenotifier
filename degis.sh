52744f0feeb5946f6b55706965eba526fafd78f7
Anıl Aras <anilaras1@gmail.com>
2023-06-10 20:23:46 +0300

    Update degis.sh

--- degis.sh
+++ degis.sh
@@ -4,7 +4,7 @@ i=1
 while [ $i -eq 1 ]
 do
 
-degis=`nslookup betrising.com 8.8.8.8|grep -i "Address" |grep -v "#"| cut -d " " -f2`
+degis=`nslookup google.com 8.8.8.8|grep -i "Address" |grep -v "#"| cut -d " " -f2`
 
 if [ $degis != "162.144.94.89" ]
 then
